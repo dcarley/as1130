8212eac5f9c1b2458001cd418fae0b8449580f07
Dan Carley <dan.carley@gmail.com>
2017-08-03 20:19:35 +0100

    Implement TextFrames
    Which generates a slice of frames that can be as a movie to scroll text
    across the device. The first frame is blank so that you can read the first
    word as it scrolls in from the side.

--- text.go
+++ text.go
@@ -1,3 +1,72 @@
 //go:generate go run embedfont/main.go -p as1130 -f "embedfont/CG pixel 4x5.ttf"
 
 package as1130
+
+import (
+	"image"
+	"image/draw"
+	"math"
+
+	"github.com/golang/freetype/truetype"
+	"golang.org/x/image/font"
+	"golang.org/x/image/math/fixed"
+)
+
+func loadFont() (font.Face, error) {
+	const size = 5
+
+	file, err := fontBytes()
+	if err != nil {
+		return nil, err
+	}
+	ttf, err := truetype.Parse(file)
+	if err != nil {
+		return nil, err
+	}
+	face := truetype.NewFace(ttf, &truetype.Options{
+		Size: float64(size),
+	})
+
+	return face, nil
+}
+
+// TextFrames renders some text into a slice of frames that can be scrolled
+// across a 24x5 display.
+func TextFrames(text string) ([]Frame24x5, error) {
+	const spacerFrames = 1
+
+	face, err := loadFont()
+	if err != nil {
+		return []Frame24x5{}, err
+	}
+
+	rect := rect24x5()
+	f := &font.Drawer{
+		Src:  image.NewUniform(On),
+		Face: face,
+		Dot:  fixed.P(rect.Max.X*spacerFrames, rect.Max.Y),
+	}
+
+	partialFrames := float64(f.MeasureString(text)>>6) / float64(rect.Max.X)
+	wholeFrames := int(math.Ceil(partialFrames)) + spacerFrames
+
+	wideFrame := Frame24x5{image.NewGray(
+		image.Rect(0, 0, rect.Max.X*wholeFrames, rect.Max.Y),
+	)}
+
+	f.Dst = wideFrame
+	f.DrawString(text)
+
+	advance := rect.Max.X
+	frames := make([]Frame24x5, wholeFrames)
+	for i, _ := range frames {
+		frame := NewFrame24x5()
+		draw.Draw(frame, frame.Bounds(), wideFrame.SubImage(rect), rect.Min, draw.Src)
+		frames[i] = frame
+
+		rect.Min.X += advance
+		rect.Max.X += advance
+	}
+
+	return frames, nil
+}
---
+++ text_test.go
@@ -0,0 +1,69 @@
+package as1130_test
+
+import (
+	. "github.com/dcarley/as1130"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Text", func() {
+	Describe("TextFrames", func() {
+		const text = "hello world as1130 test"
+		var frames []Frame24x5
+
+		BeforeEach(func() {
+			var err error
+			frames, err = TextFrames(text)
+			Expect(err).ToNot(HaveOccurred())
+		})
+
+		It("should have 1 frame per 4 characters on average", func() {
+			const (
+				framesPerChar = 4
+				spacerFrames  = 1
+			)
+
+			Expect(frames).To(HaveLen(spacerFrames + len(text)/framesPerChar))
+		})
+
+		It("should start with an empty frame", func() {
+			frame := frames[0]
+			size := frame.Bounds()
+
+			var nonEmptyPixels int
+			for x := size.Min.X; x < size.Max.X; x++ {
+				for y := size.Min.Y; y < size.Max.Y; y++ {
+					if frame.GrayAt(x, y) != Off {
+						nonEmptyPixels++
+					}
+
+				}
+			}
+
+			Expect(nonEmptyPixels).To(Equal(0), "frame 1 to have no pixels set")
+		})
+
+		It("should have have text in all other frames", func() {
+			for i, frame := range frames {
+				if i == 0 {
+					continue
+				}
+
+				size := frame.Bounds()
+				var nonEmptyPixels int
+				for x := size.Min.X; x < size.Max.X; x++ {
+					for y := size.Min.Y; y < size.Max.Y; y++ {
+						if frame.GrayAt(x, y) == On {
+							nonEmptyPixels++
+						}
+					}
+				}
+
+				Expect(nonEmptyPixels).To(BeNumerically(">", 10),
+					"frame %d to contain enough non-empty pixels", i+1,
+				)
+			}
+		})
+	})
+})
